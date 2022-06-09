25cb72e5d93c99434442026fe612f2e0fd440428
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-06-09 20:25:49 +0300

    added skeleton for blocks with pizza

--- src/components/PizzaList.jsx
+++ src/components/PizzaList.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Pizza from "./PizzaItem";
+import PizzaSkeleton from "./PizzaSkeleton";
 
 const PizzaList = ({ children }) => {
     const [pizza, setPizza] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     // move to other file component in folder API
     useEffect(() => {
         fetch("https://62a1db14cd2e8da9b0fca398.mockapi.io/pizza")
             .then((res) => res.json())
-            .then((arr) => setPizza(arr));
+            .then((arr) => {
+                setIsLoading(false);
+                setPizza(arr);
+            });
     }, []);
 
     return (
         <div>
             <h2 className="content__title">{children}</h2>
             <div className="content__items">
-                {pizza.map((data) => (
-                    <Pizza key={data.id} {...data} />
-                ))}
+                {isLoading
+                    ? [...new Array(6)].map((i) => <PizzaSkeleton key={i} />)
+                    : pizza.map((data) => <Pizza key={data.id} {...data} />)}
             </div>
         </div>
     );
